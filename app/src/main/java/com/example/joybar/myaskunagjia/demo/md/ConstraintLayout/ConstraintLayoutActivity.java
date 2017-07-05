4de72da82b649d819e081fd474403b63b83185c3
joybar <myjoybar@gmail.com>
2017-07-05 11:19:23 +0800

    Add mutiApp

--- app/src/main/java/com/example/joybar/myaskunagjia/demo/md/ConstraintLayout/ConstraintLayoutActivity.java
+++ app/src/main/java/com/example/joybar/myaskunagjia/demo/md/ConstraintLayout/ConstraintLayoutActivity.java
@@ -1,33 +1,26 @@
-package com.example.joybar.myaskunagjia.demo.md;
+package com.example.joybar.myaskunagjia.demo.md.ConstraintLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.joybar.myaskunagjia.R;
 import com.example.joybar.myaskunagjia.base.BaseActivity;
 import com.example.joybar.myaskunagjia.commom.L;
 
-
 /**
  * Created by joybar on 15/11/4.
  */
-public class MainActivityGuide extends BaseActivity {
+public class ConstraintLayoutActivity extends BaseActivity {
 
-    //http://blog.csdn.net/feiduclear_up/article/details/46457433
-
-
+    //https://juejin.im/entry/58aaedd5ac502e006974e868
     // 静态变量部分————————————————————————————————————————————————————————————————————————————————————————————————————————————————
     // public static final int CLICK_ADDRESS_ADD = 0;
     // 成员变量部分————————————————————————————————————————————————————————————————————————————————————————————————————————————————
     // 成员变量View部分————————————————————————————————————————————————————————————————————————————————————————————————————————————————
-    private Button btn,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn10,btn11,btn12,btn13,btn14,btn15,
-
-    btn16,btn17,btn18,btn19,btn20,btn21,btn22;
-
-
-    // private TextView tv_register, tv_reget_pwd;
+     private Button btn1,btn2,btn3,btn4,btn5;
+     private TextView tv1, tv2;
     // 成员变量adapter部分————————————————————————————————————————————————————————————————————————————————————————————————————————————————
     // 成员变量List部分————————————————————————————————————————————————————————————————————————————————————————————————————————————————
     // 成员变量Bean部分————————————————————————————————————————————————————————————————————————————————————————————————————————————————
@@ -38,7 +31,7 @@ public class MainActivityGuide extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guide_md);
+        setContentView(R.layout.activit_constraint_main);
         initView();
         initData();
         setLinstener();
@@ -83,9 +76,12 @@ public class MainActivityGuide extends BaseActivity {
 
     @Override
     protected void initView() {
-
-     //   btn5 = fvById(R.id.btn5);
-        // tv_register = fvById(R.id.tv_register);
+//        btn1 = fvById(R.id.btn1);
+//        btn2 = fvById(R.id.btn2);
+//        btn3 = fvById(R.id.btn3);
+//        btn4 = fvById(R.id.btn4);
+//        tv1 = fvById(R.id.tv1);
+//        tv2 = fvById(R.id.tv2);
 
     }
 
@@ -96,38 +92,10 @@ public class MainActivityGuide extends BaseActivity {
 
     @Override
     protected void setLinstener() {
-        initButton(btn, R.id.btn, com.example.joybar.myaskunagjia.demo.md.recyclerview.MainActivity.class);
-        initButton(btn2,R.id.btn2,com.example.joybar.myaskunagjia.demo.md.recyclerview_refresh.MainActivity.class);
-        initButton(btn3,R.id.btn3,com.example.joybar.myaskunagjia.demo.md.recyclerview_refresh2.MainActivity.class);
-
-
-
-        initButton(btn4,R.id.btn4,com.example.joybar.myaskunagjia.demo.md.listview.MainActivity.class);
-       initButton(btn5,R.id.btn5,com.example.joybar.myaskunagjia.demo.md.cardview.MainActivity.class);
-        initButton(btn6,R.id.btn6,com.example.joybar.myaskunagjia.demo.md.toolbar.MainActivity_Toolbar.class);
-        initButton(btn7,R.id.btn7,com.example.joybar.myaskunagjia.demo.md.FloatingActionButton.MainActivity_floating_button.class);
-
-        initButton(btn8,R.id.btn8,com.example.joybar.myaskunagjia.demo.md.TabLayout.MainActivity_TabLayout.class);
-        initButton(btn9,R.id.btn9,com.example.joybar.myaskunagjia.demo.md.CoordinatorLayout.MainActivity.class);
-        initButton(btn10,R.id.btn10,com.example.joybar.myaskunagjia.demo.md.NavigationView.MainActivity_navigation.class);
-        initButton(btn11,R.id.btn11,com.example.joybar.myaskunagjia.demo.md.CoordinatorLayout2.MainActivity.class);
-        initButton(btn12,R.id.btn12,com.example.joybar.myaskunagjia.demo.md.CoordinatorLayout3.MainActivity.class);
-
-        initButton(btn13,R.id.btn13,com.example.joybar.myaskunagjia.demo.md.CoordinatorLayout4.MainActivity.class);
-
-        initButton(btn14,R.id.btn14,com.example.joybar.myaskunagjia.demo.md.recyclerview_refresh3.MainActivity.class);
-        initButton(btn15,R.id.btn15,com.example.joybar.myaskunagjia.demo.md.recyclerview_refresh4.MainActivity.class);
-
-        initButton(btn16,R.id.btn16,com.example.joybar.myaskunagjia.demo.md.SwipeDismissBehavior.MainActivity.class);
-
-       initButton(btn17,R.id.btn17,com.example.joybar.myaskunagjia.demo.md.CoordinatorLayout6.MainActivity.class);
-       initButton(btn18,R.id.btn18,com.example.joybar.myaskunagjia.demo.md.RecyclerViewHeader.MainActivity.class);
-       initButton(btn19,R.id.btn19,com.example.joybar.myaskunagjia.demo.md.CoordinatorLayout7.MainActivity.class);
-       initButton(btn20,R.id.btn20,com.example.joybar.myaskunagjia.demo.md.sortRecyclerView.MainActivity.class);
-       initButton(btn21,R.id.btn21,com.example.joybar.myaskunagjia.demo.md.mddemo.activity.SignInActivity.class);
-       initButton(btn21,R.id.btn21,com.example.joybar.myaskunagjia.demo.md.mddemo.activity.SignInActivity.class);
-       initButton(btn22,R.id.btn22,com.example.joybar.myaskunagjia.demo.md.ConstraintLayout.ConstraintLayoutActivity.class);
-
+//         btn1.setOnClickListener(this);
+//         btn2.setOnClickListener(this);
+//         btn3.setOnClickListener(this);
+//         btn4.setOnClickListener(this);
 
     }
 
@@ -143,23 +111,27 @@ public class MainActivityGuide extends BaseActivity {
     //回调方法部分————————————————————————————————————————————————————————————————————————————————————————————————————————————————
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+             case R.id.btn1:
 
+              break;
+            case R.id.btn2:
+
+                break;
+            case R.id.btn3:
+
+                break;
+            case R.id.btn4:
+
+                break;
+
+            default:
+                break;
+        }
 
     }
 
-    public void initButton(Button btn, int id, final Class c) {
 
-        btn = (Button) this.findViewById(id);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                Intent intent = new Intent(MainActivityGuide.this, c);
-                startActivity(intent);
-
-            }
-        });
-    }
 
 
 }
---
+++ multiapp/src/main/java/com/joybar/multiapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.joybar.multiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+	private TextView tv;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		tv = (TextView) findViewById(R.id.tv);
+		boolean deBugModel = BuildConfig.BUILD_TYPE_DEBUG;
+		String deBugModelType = BuildConfig.BUILD_TYPE_MODE;
+		tv.setText("deBugModel=" + deBugModel + "/n" + ",deBugModelType=" + deBugModelType);
+	}
+}
