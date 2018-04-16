8aad82bcf908904f60ad83df77d9d1886279cf32
joybar <myjoybar@gmail.com>
2018-04-16 11:33:27 +0800

    Revert "update .gitignore"
    This reverts commit fb6c03b365bec1a9dd922145b522d737f136d69a.

--- aop/src/main/java/test/joybar/com/aop/MainActivity.java
+++ aop/src/main/java/test/joybar/com/aop/MainActivity.java
@@ -1,54 +1,50 @@
 package test.joybar.com.aop;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.View;
 
-import test.joybar.com.aop.login.CheckLogin;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final String TAG = "CheckLoginAspectJ";
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		loginCheck();
+		netCheck();
+		permissionCheck();
+	}
+	private void loginCheck(){
+		findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.btn_login).setOnClickListener(this);
-        findViewById(R.id.btn_net).setOnClickListener(this);
-        findViewById(R.id.btn_permission).setOnClickListener(this);
-    }
+			}
+		});
 
 
-    @CheckLogin
-    private void loginCheck() {
-        Log.i(TAG, "已经登陆，执行登陆后的逻辑");
-    }
+	}
+	private void netCheck(){
 
-    private void netCheck() {
-    }
 
-    private void permissionCheck() {
+		findViewById(R.id.btn_net).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
 
-    }
-   // @CheckLogin
-    @Override
-    public void onClick(View v) {
-        String result = "";
-        switch (v.getId()) {
+			}
+		});
 
-            case R.id.btn_login:
-                loginCheck();
-                break;
-            case R.id.btn_net:
-                netCheck();
-                break;
-            case R.id.btn_permission:
-                netCheck();
-                break;
-            default:
-                break;
-        }
 
-    }
+
+	}
+
+	private void permissionCheck(){
+
+		findViewById(R.id.btn_permission).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+	}
 }
--- aop/src/main/java/test/joybar/com/aop/login/CheckLoginAspectJ.java
+++ aop/src/main/java/test/joybar/com/aop/login/CheckLoginAspectJ.java
@@ -1,11 +1,7 @@
 package test.joybar.com.aop.login;
 
-import android.app.Activity;
-import android.app.Application;
-import android.app.Fragment;
 import android.content.Context;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -37,16 +33,13 @@ public class CheckLoginAspectJ {
 	 */
 	@Around("executionCheckLogin()")
 	public Object checkLogin(ProceedingJoinPoint joinPoint) throws Throwable {
-		Log.i(TAG, "start checkLogin: ");
+		Log.i(TAG, "checkLogin: ");
 		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
 		CheckLogin checkLogin = signature.getMethod().getAnnotation(CheckLogin.class);
 		if (checkLogin != null) {
-			Context context = getContext(joinPoint.getThis()) ;
-			java.util.Random random=new java.util.Random();
-			int result=random.nextInt(2)+1;// 返回[0,2)
-			if (result==1) {
+			Context context = (Context) joinPoint.getThis();
+			if (true) {
 				Log.i(TAG, "checkLogin: 登录成功 ");
-				Toast.makeText(context,"登录成功",Toast.LENGTH_SHORT).show();;
 				return joinPoint.proceed();
 			} else {
 				Log.i(TAG, "checkLogin: 请登录");
@@ -55,29 +48,5 @@ public class CheckLoginAspectJ {
 			}
 		}
 		return joinPoint.proceed();
-	}
-
-	/**
-	 * 通过对象获取上下文
-	 *
-	 * @param object
-	 * @return
-	 */
-	private Context getContext(Object object) {
-		if (object instanceof Activity) {
-			return (Activity) object;
-		}else if (object instanceof Application) {
-			return (Application) object;
-		} else if (object instanceof Fragment) {
-			android.app.Fragment fragment = (android.app.Fragment) object;
-			return fragment.getActivity();
-		}  else if (object instanceof Fragment) {
-			android.support.v4.app.Fragment fragment = (android.support.v4.app.Fragment) object;
-			return fragment.getActivity();
-		}else if (object instanceof View) {
-			View view = (View) object;
-			return view.getContext();
-		}
-		return null;
 	}
 }
